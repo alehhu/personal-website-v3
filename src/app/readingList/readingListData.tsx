f5c18e2f43b0cac561add13d92661d59af4559a8
alehhu <159355663+alehhu@users.noreply.github.com>
2024-09-07 14:09:18 +0200

    added data

--- src/app/readingList/readingListData.tsx
+++ src/app/readingList/readingListData.tsx
@@ -5,9 +5,29 @@ numerical valuation, 1)difficulty, 2)learned a lot, 3)enjoyed course, 4)timely g
 */
 export const readingListCollection: Book[] = new Array(
     new Book(
+        "Thus Spoke Zaratustra",
+        "Nietzsche",
+        "motivation and courage"
+    ),
+    new Book(
+        "How to Win Friends and Influence People",
+        "Carnegie",
+        "from an awkward guy myself, it taught me how to be a better person"
+    ),
+    new Book(
+        "The Innovator's Dilemma",
+        "Christensen",
+        "shows weaknesses of big enterprise in tackling innovation"
+    ),
+    new Book(
+        "The Lean Startup",
+        "Eric Ries",
+        "it gives execution practical principles for startups"
+    ),
+    new Book(
         "Distributed Systems Principles",
         "Tanenbaum",
-        "great and interesting explanation of modern software architecture"
+        "great and interesting explanation of modern software architecture (gave it a fast read)"
     ),
     new Book(
         "Benjamin Franklin: An American Life",
